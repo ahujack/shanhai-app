31b9f4fc040322c35e9e9a8b2c544f4300d85a32
王杰 <wangjie@wangjiedeMacBook-Pro.local>
2026-03-03 14:09:19 +0800

    fix: ChartModule 导入 UserModule 解决依赖问题

--- shanhai-server/src/modules/chart/chart.module.ts
+++ shanhai-server/src/modules/chart/chart.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChartService } from './chart.service';
 import { ChartController } from './chart.controller';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [ChartController],
   providers: [ChartService],
   exports: [ChartService],
